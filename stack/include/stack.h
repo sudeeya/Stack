80a43696f4579ce275c339c9ba4656af1c78e3d2
sudeeya <pyrkin.aa@phystech.edu>
2021-11-04 22:03:28 +0300

    Add cmake and tests with gtest

--- stack/include/stack.h
+++ stack/include/stack.h
@@ -5,77 +5,77 @@
 
 namespace stack_project {
 
-	template <class T>
-	class Stack {
-	public:
-		const size_t InitialCapacity = 32;
-		const size_t MemoryMultiplier = 2;
+  template <class T>
+  class Stack {
+  public:
+    const size_t InitialCapacity = 32;
+    const size_t MemoryMultiplier = 2;
 
-		Stack();
-		Stack(const T* data, size_t size);
-		Stack(const Stack<T>& other);
-		Stack(Stack<T>&& other);
+    Stack();
+    Stack(const T* data, size_t size);
+    Stack(const Stack<T>& other);
+    Stack(Stack<T>&& other);
 
-		Stack<T>& operator=(const Stack<T>& other);
-		Stack<T>& operator=(Stack<T>&& other);
+    Stack<T>& operator=(const Stack<T>& other);
+    Stack<T>& operator=(Stack<T>&& other);
 
-		~Stack();
+    ~Stack();
 
-		void push(T data);
-		void pop();
-		T& top();
-		size_t size() const;
-		size_t capacity() const;
-		bool is_empty() const;
+    void push(T data);
+    void pop();
+    T& top();
+    size_t size() const;
+    size_t capacity() const;
+    bool is_empty() const;
 
-		bool operator==(const Stack<T>& other) const;
-		bool operator!=(const Stack<T>& other) const;
+    bool operator==(const Stack<T>& other) const;
+    bool operator!=(const Stack<T>& other) const;
 
-	private:
-		void data_realloc();
+  private:
+    void data_realloc();
 
-		T* data_;
-		size_t capacity_;
-		size_t size_;
-	};
-	
-	template <>
-	class Stack<bool> {
-	public:
-		const size_t InitialCapacity = 32;
-		const size_t MemoryMultiplier = 2;
-		const size_t SizeOfChar = 8;
+    T* data_;
+    size_t capacity_;
+    size_t size_;
+  };
+  
+  template <>
+  class Stack<bool> {
+  public:
+    const size_t InitialCapacity = 32;
+    const size_t MemoryMultiplier = 2;
+    const size_t SizeOfChar = 8;
 
-		Stack();
-		Stack(const bool* data, size_t size);
-		Stack(const Stack<bool>& other);
-		Stack(Stack<bool>&& other);
+    Stack();
+    Stack(const bool* data, size_t size);
+    Stack(const Stack<bool>& other);
+    Stack(Stack<bool>&& other);
 
-		Stack<bool>& operator=(const Stack<bool>& other);
-		Stack<bool>& operator=(Stack<bool>&& other);
+    Stack<bool>& operator=(const Stack<bool>& other);
+    Stack<bool>& operator=(Stack<bool>&& other);
 
-		~Stack();
+    ~Stack();
 
-		void push(bool data);
-		void pop();
-		bool top() const;
-		size_t size() const;
-		size_t capacity() const;
-		bool is_empty() const;
+    void push(bool data);
+    void pop();
+    bool top() const;
+    size_t size() const;
+    size_t capacity() const;
+    bool is_empty() const;
 
-		bool operator==(const Stack<bool>& other) const;
-		bool operator!=(const Stack<bool>& other) const;
+    bool operator==(const Stack<bool>& other) const;
+    bool operator!=(const Stack<bool>& other) const;
 
-		size_t get_byte_capacity() const;
+    size_t get_byte_capacity() const;
 
-	private:
-		void data_realloc();
+  private:
+    void data_realloc();
 
-		unsigned char* data_;
-		size_t capacity_;
-		size_t size_;
-	};
-	
+    unsigned char* data_;
+    size_t capacity_;
+    size_t size_;
+  };
+  
 } // namespace stack_project
 
 #endif // STACK_PROJECT_STACK_INCLUDE_STACK_H_
---
+++ stack/test/tests.cpp
@@ -0,0 +1,174 @@
+#include <gtest/gtest.h>
+#include "../include/stack_impl.h"
+
+namespace {
+
+using stack_project::Stack;
+
+TEST(StackTest, DefaultConstructor) { 
+  Stack<int> stack;
+  EXPECT_EQ(0, stack.size());
+}
+
+TEST(StackTest, ConstructorFromArray) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> stack{ data, size };
+  Stack<bool> b_stack{ b_data, size };
+  EXPECT_EQ(size, stack.size());
+  EXPECT_EQ(size, b_stack.size());
+  for (size_t i = 0; i < size; ++i) {
+    EXPECT_EQ(data[size - i - 1], stack.top());
+    EXPECT_EQ(b_data[size - i - 1], b_stack.top());
+    stack.pop();
+    b_stack.pop();
+  }
+}
+
+TEST(StackTest, CopyConstructor) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> other_stack{ data, size }, stack{ other_stack };
+  Stack<bool> other_b_stack{ b_data, size }, b_stack{ other_b_stack };
+  EXPECT_EQ(other_stack, stack);
+  EXPECT_EQ(other_b_stack, b_stack);
+}
+
+TEST(StackTest, MoveConstructor) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> other_stack{ data, size }, other_stack_copy{ other_stack }, stack{ std::move(other_stack) };
+  Stack<bool> other_b_stack{ b_data, size }, other_b_stack_copy{ other_b_stack }, b_stack{ std::move(other_b_stack) };
+  EXPECT_EQ(other_stack_copy, stack);
+  EXPECT_EQ(other_b_stack_copy, b_stack);
+}
+
+TEST(StackTest, CopyAssignmentOperator) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> other_stack{ data, size }, stack;
+  Stack<bool> other_b_stack{ b_data, size }, b_stack;
+  stack = other_stack;
+  b_stack = other_b_stack;
+  EXPECT_EQ(other_stack, stack);
+  EXPECT_EQ(other_b_stack, b_stack);
+}
+
+TEST(StackTest, MoveAssignmentOperator) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> other_stack{ data, size }, other_stack_copy{ other_stack }, stack;
+  Stack<bool> other_b_stack{ b_data, size }, other_b_stack_copy{ other_b_stack }, b_stack;
+  stack = std::move(other_stack);
+  b_stack = std::move(other_b_stack);
+  EXPECT_EQ(other_stack_copy, stack);
+  EXPECT_EQ(other_b_stack_copy, b_stack);
+}
+
+TEST(StackTest, Push) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> other_stack{ data, size }, stack{ data, size - 1 };
+  Stack<bool> other_b_stack{ b_data, size }, b_stack{ b_data, size - 1 };
+  stack.push(other_stack.top());
+  b_stack.push(other_b_stack.top());
+  EXPECT_EQ(other_stack, stack);
+  EXPECT_EQ(other_b_stack, b_stack);
+}
+
+TEST(StackTest, Pop) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> other_stack{ data, size - 1 }, stack{ data, size };
+  Stack<bool> other_b_stack{ b_data, size - 1 }, b_stack{ b_data, size };
+  stack.pop();
+  b_stack.pop();
+  EXPECT_EQ(other_stack, stack);
+  EXPECT_EQ(other_b_stack, b_stack);
+}
+
+TEST(StackTest, Top) {
+  const size_t size = 3;
+  const int new_top = 4;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> stack{ data, size };
+  Stack<bool> b_stack{ b_data, size };
+  EXPECT_EQ(data[size - 1], stack.top());
+  stack.top() = new_top;
+  EXPECT_EQ(new_top, stack.top());
+  EXPECT_EQ(b_data[size - 1], b_stack.top());
+}
+
+TEST(StackTest, Size) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> stack{ data, size };
+  Stack<bool> b_stack{ b_data, size };
+  EXPECT_EQ(size, stack.size());
+  EXPECT_EQ(size, b_stack.size());
+}
+
+TEST(StackTest, Capacity) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> empty_stack, stack{ data, size };
+  Stack<bool> b_stack{ b_data, size };
+  EXPECT_EQ(empty_stack.InitialCapacity, stack.capacity());
+  EXPECT_EQ(stack.InitialCapacity, stack.capacity());
+  EXPECT_EQ(b_stack.InitialCapacity, b_stack.capacity());
+  const size_t big_size = 40;
+  int big_data[big_size] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+                            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+                            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+                            1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+  Stack<int> big_stack{ big_data, big_size };
+  EXPECT_EQ(2 * big_stack.InitialCapacity, big_stack.capacity());
+}
+
+TEST(StackTest, IsEmpty) {
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> empty_stack, stack{ data, size };
+  Stack<bool> b_stack{ b_data, size };
+  EXPECT_EQ(true, empty_stack.is_empty());
+  EXPECT_EQ(false, stack.is_empty());
+  EXPECT_EQ(false, b_stack.is_empty());
+}
+
+TEST(StackTest, EqualityOperator) { 
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> lhs{ data, size }, rhs{ data, size };
+  Stack<bool> b_lhs{ b_data, size }, b_rhs{ b_data, size - 1};
+  EXPECT_TRUE(lhs == rhs);
+  EXPECT_FALSE(b_lhs == b_rhs);
+}
+
+TEST(StackTest, InequalityOperator) { 
+  const size_t size = 3;
+  int data[size] = { 5, 6, 3 };
+  bool b_data[size] = { false, true, true };
+  Stack<int> lhs{ data, size }, rhs{ data, size - 1 };
+  Stack<bool> b_lhs{ b_data, size }, b_rhs{ b_data, size };
+  EXPECT_TRUE(lhs != rhs);
+  EXPECT_FALSE(b_lhs != b_rhs);
+}
+
+} // namespace
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
